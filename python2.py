a2c4eca1c6fba649b28a93baf09a6c43f42bb713
sophida <sophidachaphet@gmail.com>
2026-01-31 13:55:55 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,55 @@
+"""
+#
+# Part: Python Operators
+#
+"""
+x = 4 + 5 #Addition
+y = 10 - 2 #Subtraction
+z = 3 * 7 #Multiplication
+a = 8 / 2 #Division
+b = 10 % 3 #Modulus
+c = 2 ** 3 #Exponentiation
+
+
+"""
+#
+# Part: Python Operators
+# Assignment Operators
+#
+"""
+x = 5
+x = x + 3
+x += 3
+
+"""
+#
+# Part: Python Operators
+# Comparison Operators
+#
+"""
+a = 5
+b = 10
+print(a == b)  # Equal to
+print(a != b)  # Not equal to
+print(a > b)   # Greater than
+print(a < b)   # Less than
+print(a >= b)  # Greater than or equal to
+print(a <= b)  # Less than or equal to
+
+"""
+#
+# Part: Python Operators
+# Logical Operators
+#
+"""
+x = True
+y = False
+print(x and y)  # Logical AND
+print(x or y)   # Logical OR
+print(not x)    # Logical NOT
+print(x & y)  # Bitwise AND
+print(x | y)   # Bitwise OR
+print(~x)    # Bitwise NOT
+a = 5
+b = 10
+print(a < 10 and b > 5)  # Logical AND with comparisons
